d2373f0fc0a5765ad97dccfd3eeb67705913839d
Hammad Naeem <hammaddotio@gmail.com>
2025-05-07 09:44:56 +0500

    update vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< Updated upstream
-  plugins: [react()],
-  base: 'https://hammaddotio.github.io/silly-smile-wallpaper-images/'
-=======
   plugins: [
     react(),
     tailwindcss(),
   ],
   base: '/silly-smile-wallpaper-images/'
->>>>>>> Stashed changes
 })
