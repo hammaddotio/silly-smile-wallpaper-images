c3e47dbc8a2ab520a71d4f589dc41f2be5b0e5ce
Hammad Naeem <hammaddotio@gmail.com>
2025-05-07 09:42:16 +0500

    add tailwind and som config for gh pages

---
+++ src/app/Pages/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeScreen = () => {
+    return (
+        <h1 class="">
+            Hello world!
+        </h1>
+    )
+}
+
+export default HomeScreen
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+<<<<<<< Updated upstream
   plugins: [react()],
   base: 'https://hammaddotio.github.io/silly-smile-wallpaper-images/'
+=======
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/silly-smile-wallpaper-images/'
+>>>>>>> Stashed changes
 })
